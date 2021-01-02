4273fcf360bd0e0c764ad27b7640d43c51ee6115
Arthurdw <mail@arthurdw.com>
2021-01-02 10:35:52 +0100

    ✨ Fixed little problem with main element position.

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -2,17 +2,23 @@ import React, { Suspense } from "react";
 
 import { Helmet } from "react-helmet";
 import { Navigation } from "../components/navigation/Navigation.comp";
+import styled from "styled-components";
 
 export interface DefaultLayoutProps {
   title?: string;
 }
 
+const MainContent = styled.main`
+  position: absolute;
+  top: 90px;
+`;
+
 export const DefaultLayout: React.FC<DefaultLayoutProps> = (props) => (
   <Suspense fallback={<p>Loading</p>}>
-      <Helmet>
-          <title>GO-PC Build{props.title && ` | ${props.title}`}</title>
-      </Helmet>
-      <Navigation />
-      <main>{props.children}</main>
+    <Helmet>
+      <title>GO-PC Build{props.title && ` | ${props.title}`}</title>
+    </Helmet>
+    <Navigation />
+    <MainContent>{props.children}</MainContent>
   </Suspense>
 );
