0c91bba903bd646ddb2c842fe403c25c94d0df8a
Arthurdw <arthur.dewitte@gmail.com>
2021-01-06 11:08:23 +0100

    💬 Fixed typo.

--- src/components/intro/Intro.comp.tsx
+++ src/components/intro/Intro.comp.tsx
@@ -6,7 +6,7 @@ export const Intro: React.FC = () => (
   <IntroWrapper>
     <Title>Welkom bij{'\n'}GO-PC Build</Title>
     <Description>
-      Leer bij ons proffesioneel een pc bouwen! Van het samenstellen tot het
+      Leer bij ons professioneel een pc bouwen! Van het samenstellen tot het
       bestuurings systeem installeren!
     </Description>
     <Reserve to="/reserveer">Reserveer nu</Reserve>
