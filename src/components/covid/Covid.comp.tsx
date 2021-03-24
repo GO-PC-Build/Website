657abaf07eaee1a342a2f43780f43a40d77ba82a
Arthurdw <mail@arthurdw.com>
2021-03-24 20:34:04 +0100

    👀 New epic government rules implemented. ❤ Thanks government

--- src/components/covid/Covid.comp.tsx
+++ src/components/covid/Covid.comp.tsx
@@ -30,7 +30,7 @@ export const Covid: React.FC<CovidProps> = (props) => (
     </CovidPeekWrapper>
     <CovidSplitter />
     <CovidMessage>
-      Door de coronamaatregelen zijn er regels waaraan we ons moeten
+      {/* Door de coronamaatregelen zijn er regels waaraan we ons moeten
       houden binnen het GO onderwijs: <br />
       &nbsp;&nbsp;&nbsp;&nbsp;31 maart: (1ste & 3de graad op school)
       <br />
@@ -40,10 +40,16 @@ export const Covid: React.FC<CovidProps> = (props) => (
       <br />
       &nbsp;&nbsp;&nbsp;&nbsp;12:30u - 14:00u
       <br />
-      &nbsp;&nbsp;&nbsp;&nbsp;eten kan nog buiten van 12:00u - 12:30u
+      &nbsp;&nbsp;&nbsp;&nbsp;eten kan nog buiten van 12:00u - 12:30u */}
+      Door gewijzigde Corona maatregelen zijn de huidige workshop datums
+      onbekend. We houden jullie op de hoogte via onze smartschool.
       <br />
       <br />
-      Je kan ons draaiboek op <a href="/draaiboek" target="_blanc">gpb.go-ao.be/draaiboek</a> vinden.
+      Je kan ons draaiboek op{" "}
+      <a href="/draaiboek" target="_blanc">
+        gpb.go-ao.be/draaiboek
+      </a>{" "}
+      vinden.
     </CovidMessage>
   </CovidWrapper>
 );
--- src/pages/reservation.tsx
+++ src/pages/reservation.tsx
@@ -1,71 +1,78 @@
-import { Description, Title } from "../components/intro/Intro.styled";
-import React, { useContext, useEffect, useState } from "react";
+// import { Description, Title } from "../components/intro/Intro.styled";
+// import React, { useContext, useEffect, useState } from "react";
 
-import { AccountContext } from "../layouts/DefaultLayout";
-import { Board } from "./reserve";
-import axios from "axios";
+import React from "react";
+import { Title } from "../components/intro/Intro.styled";
 
-interface Data {
-  workshop: number;
-  location: number | undefined;
-  board: number[];
-}
+// import { AccountContext } from "../layouts/DefaultLayout";
+// import { Board } from "./reserve";
+// import axios from "axios";
+
+// interface Data {
+//   workshop: number;
+//   location: number | undefined;
+//   board: number[];
+// }
 
 export const ReservationPage: React.FC = () => {
-  const [data, setData] = useState<Data>({
-    workshop: 0,
-    location: undefined,
-    board: [],
-  });
-  const { internalnr } = useContext(AccountContext);
+  // const [data, setData] = useState<Data>({
+  //   workshop: 0,
+  //   location: undefined,
+  //   board: [],
+  // });
+  // const { internalnr } = useContext(AccountContext);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (internalnr === 0) return;
-      const urlBase =
-        process.env.NODE_ENV === "development"
-          ? "http://localhost:25578"
-          : "https://api.arthurdw.com/go-pc-build";
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     if (internalnr === 0) return;
+  //     const urlBase =
+  //       process.env.NODE_ENV === "development"
+  //         ? "http://localhost:25578"
+  //         : "https://api.arthurdw.com/go-pc-build";
 
-      const user = await axios.get(`${urlBase}/user/id/${internalnr}`);
-      const schemes = await axios.get(`${urlBase}/schemes`);
+  //     const user = await axios.get(`${urlBase}/user/id/${internalnr}`);
+  //     const schemes = await axios.get(`${urlBase}/schemes`);
 
-      setData({
-        workshop: user.data.workshop,
-        location: user.data.location,
-        board: schemes.data.schemes[user.data.workshop].filter(
-          (i: number) => i !== user.data.location
-        ),
-      });
-    };
+  //     setData({
+  //       workshop: user.data.workshop,
+  //       location: user.data.location,
+  //       board: schemes.data.schemes[user.data.workshop].filter(
+  //         (i: number) => i !== user.data.location
+  //       ),
+  //     });
+  //   };
 
-    fetchData();
-  }, [internalnr]);
+  //   fetchData();
+  // }, [internalnr]);
 
   return (
-    <React.Fragment>
-      <Title
-        style={{
-          margin: "15px auto",
-          textAlign: "center",
-        }}
-      >
-        Jouw reservatie <span style={{ fontSize: "75%" }}>(groen)</span>
-      </Title>
-      <Description
-        style={{
-          margin: "15px auto",
-          textAlign: "center",
-        }}
-      >
-        {data.workshop === 0 ? "31 maart" : "21 april"}
-      </Description>
-      <Board
-        data={data.board}
-        selected={data.location}
-        setSelected={() => {}}
-      />
-    </React.Fragment>
+    // <React.Fragment>
+    //   <Title
+    //     style={{
+    //       margin: "15px auto",
+    //       textAlign: "center",
+    //     }}
+    //   >
+    //     Jouw reservatie <span style={{ fontSize: "75%" }}>(groen)</span>
+    //   </Title>
+    //   <Description
+    //     style={{
+    //       margin: "15px auto",
+    //       textAlign: "center",
+    //     }}
+    //   >
+    //     {data.workshop === 0 ? "31 maart" : "21 april"}
+    //   </Description>
+    //   <Board
+    //     data={data.board}
+    //     selected={data.location}
+    //     setSelected={() => {}}
+    //   />
+    // </React.Fragment>
+    <Title>
+      Door gewijzigde Corona maatregelen zijn de huidige workshop datums
+      onbekend. We houden jullie op de hoogte via onze smartschool.
+    </Title>
   );
 };
 
